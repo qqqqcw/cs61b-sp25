3ffbeb2a4b6d49ebfa655306cc4e3791824f5e16
qqqcw <2949135468@qq.com>
2025-08-11 18:21:06 +0800

    finished guitarhero

--- proj1b/src/gh2/GuitarString.java
+++ proj1b/src/gh2/GuitarString.java
@@ -1,6 +1,9 @@
 package gh2;
 
 import deque.Deque61B;
+import deque.LinkedListDeque61B;
+
+import java.util.Map;
 
 // TODO: maybe more imports
 
@@ -14,7 +17,7 @@ public class GuitarString {
 
     /* Buffer for storing sound data. */
     // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque61B<Double> buffer;
+     private Deque61B<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
@@ -22,6 +25,11 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer with zeros.
+        int capacity = (int) Math.round(SR / frequency);
+        buffer = new LinkedListDeque61B<>();
+        for (int i = 0; i < capacity; i++) {
+            buffer.addFirst(0.0);
+        }
     }
 
 
@@ -35,6 +43,11 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+        int capacity = buffer.size();
+        for (int i = 0; i < capacity; i++) {
+            buffer.addFirst(Math.random() - 0.5);
+            buffer.removeLast();
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
@@ -44,12 +57,17 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       **Do not call StdAudio.play().**
+        double first = buffer.removeFirst();
+        double second = buffer.get(0);
+        double newItem = (first + second) / 2 * DECAY;
+        buffer.addLast(newItem);
+
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.get(0);
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
