a0c82173f3a9d6df076a856c63b80be30912f61e
qqqcw <2949135468@qq.com>
2026-03-14 00:03:44 +0800

    hw02 finished

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -3,33 +3,71 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     // TODO: Add any necessary instance variables.
+    private boolean[][] grid;
+    private int n;
+    private int num;
+    private WeightedQuickUnionUF uf;
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
 
+    private int transNum(int row, int col) {
+        return row * n + col + 1;
+    }
+
+    private boolean isSafe(int row, int col) {
+        return row < n && row >= 0 && col < n && col >= 0;
+    }
     public Percolation(int N) {
         // TODO: Fill in this constructor.
+        this.grid = new boolean[N][N];
+        this.n = N;
+        uf = new WeightedQuickUnionUF(N * N + 2);
     }
 
     public void open(int row, int col) {
         // TODO: Fill in this method.
+        if (isOpen(row, col)) {
+            return;
+        }
+        if(row == 0) {
+            uf.union(transNum(row, col), 0);
+        }
+        if (row == n - 1) {
+            uf.union(transNum(row, col), n * n + 1);
+        }
+        grid[row][col] = true;
+        num += 1;
+        for (int i = 0; i < 4; i++) {
+            int newRow = row + dr[i];
+            int newCol= col + dc[i];
+            if (isSafe(newRow, newCol) && isOpen(newRow, newCol)) {
+                uf.union(transNum(row, col), transNum(newRow, newCol));
+            }
+        }
+
     }
 
     public boolean isOpen(int row, int col) {
         // TODO: Fill in this method.
-        return false;
+        return grid[row][col];
     }
 
     public boolean isFull(int row, int col) {
         // TODO: Fill in this method.
-        return false;
+        // 目前的实现有问题，若虚拟顶部与虚拟底部已经连通，任意一不渗透的点
+        // 与虚拟底部连通时，却被判定为full，可以用双uf解决，另一个uf
+        // 只有虚拟顶部，仅用于isFull方法，暂未实现
+        return uf.connected(transNum(row, col), 0);
     }
 
     public int numberOfOpenSites() {
         // TODO: Fill in this method.
-        return 0;
+        return this.num;
     }
 
     public boolean percolates() {
         // TODO: Fill in this method.
-        return false;
+        return uf.connected(0, n * n + 1);
     }
 
     // TODO: Add any useful helper methods (we highly recommend this!).
